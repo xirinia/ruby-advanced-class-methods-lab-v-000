46b0000d2ad441134b678e1e1a13c124ea8e3283
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-16 01:28:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,47 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+       song
+    else
+      self.create_by_name(name)
+      
+    end
+  end
+
+    def self.alphabetical
+      name_array = []
+      @@all.each do |songs|
+        name_array << songs
+      end
+      name_array.sort_by {|song| song.name}
+    end
+
+    def self.create_from_filename
+
+    end
 end
