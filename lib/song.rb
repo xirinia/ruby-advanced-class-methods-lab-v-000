ec7a17579c6d1241b9f32052893c2814216bdf1c
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-16 01:48:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,6 +55,7 @@ class Song
       song = self.new
       song.name = split[0].chop
       song.artist_name = split[1].chomp(".mp3")
+      @@all << song
       end
 
   def self.create_from_filename(string)
